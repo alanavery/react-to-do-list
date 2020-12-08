e362aa723c2d70cd9560643e240273820cc3fbc6
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-08 16:32:20 -0500

    changed from functional component to class component and add styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import ListItem from './ListItem';
-
+import logo from "./logo.svg";
+import "./App.css";
+import ListItem from "./components/ListItem";
 
 function App() {
-  let list = ['Buy a standup desk', 'Get a job', 'Redo resume, again'];
+  let list = ["Buy a standup desk", "Get a job", "Redo resume, again"];
   let listItems = list.map((toDo, index) => {
     return <ListItem doThis={toDo} key={index} />;
   });
   return (
-    <div>
-      <header>My List</header>
-      <div>
-        <ul>
-          <ListItem doThis="Make the list" />
-          <ListItem doThis="Finish the deliverable" />
-          <ListItem doThis="Clean the bathroom" />
-          <ListItem doThis="Turn in your resume" />
-          {listItems}
-        </ul>
-      </div>
+    <div className="App-header">
+      <header >
+        My List
+        <div>
+          <ul>
+            <ListItem doThis="Make the list" />
+            <ListItem doThis="Finish the deliverable" />
+            <ListItem doThis="Clean the bathroom" />
+            <ListItem doThis="Turn in your resume" />
+            {listItems}
+          </ul>
+        </div>
+      </header>
     </div>
   );
 }
